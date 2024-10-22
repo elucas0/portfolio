78217df3840d4e8d750493f52e78f0e292546cae
Elouann Lucas <elouannlucas56@gmail.com>
2024-10-22 13:18:31 +0200

    Added core components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import DownloadCVButton from './DownloadCVButton.vue';
+
+
+// defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <div class="flex gap-5 bg-slate-400">
+    <button type="button" @click="">About</button>
+    <button type="button" @click="">My Projects</button>
+    <button type="button" @click="">Contact Me</button>
+    <DownloadCVButton />
+  </div>
+</template>
---
+++ src/components/Technobox.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+defineProps<{ logoSrc: string, altText: string }>()
+</script>
+
+<template>
+    <a href="https://vitejs.dev" target="_blank">
+        <img :src=logoSrc width="40" :alt=altText />
+    </a>
+</template>
